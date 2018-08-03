2b13bb960c955181a88f48c4faddfbaabdb6a321
Anuj Dutt <anuj.dutt9@gmail.com>
2018-08-03 07:04:20 -0400

    Added Notes and Project: Custom Node Design Files

---
+++ Custom_Node_Design/test/Inbox.test.js
@@ -0,0 +1,13 @@
+// This file contains the Javascript code to test out the Solidity contract that is being compiled by the
+// compile.js script. This script helps to automatically test the Solidity Contract for a range of different Inputs
+// instead of entering them in one by one and testing for them using Remix.
+
+// Import libraries
+const assert = require('assert');
+
+// Import Ganache-cli: Local Etherium Test Network
+const ganache = require('ganache-cli');
+
+// Import Web3
+/// Web3: a Constructor function
+const Web3 = require('web3');
